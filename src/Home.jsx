0cb6b5f1466fc462ae65fa7070c0b2548783e04c
Colton Peckham <coltonpeckham@gmail.com>
2023-01-05 14:38:30 -0600

    added signup,login, and logout capabilities

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,6 +3,9 @@ import { useEffect, useState } from "react";
 import { AnimesIndex } from "./AnimesIndex";
 import { AnimesNew } from "./AnimesNew";
 import { AnimesShow } from "./AnimesShow";
+import { Signup } from "./Signup";
+import { Login } from "./Login";
+import { LogoutLink } from "./LogoutLink";
 import { Modal } from "./Modal";
 
 export function Home() {
@@ -70,6 +73,9 @@ export function Home() {
 
   return (
     <div>
+      <Signup />
+      <Login />
+      <LogoutLink />
       <AnimesNew onCreateAnime={handleCreateAnime} />
       <AnimesIndex animes={animes} onShowAnime={handleShowAnime} />
       <Modal show={IsAnimesShowVisible} onClose={handleClose}>
@@ -80,117 +86,5 @@ export function Home() {
         />
       </Modal>
     </div>
-  );
-}
-
-export function Signup() {
-  const [errors, setErrors] = useState([]);
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    setErrors([]);
-    const params = new FormData(event.target);
-    axios
-      .post("http://localhost:3000/users", params)
-      .then((response) => {
-        console.log(response.data);
-        event.target.reset();
-        window.location.href = "/"; // Change this to hide a modal, redirect to a specific page, etc.
-      })
-      .catch((error) => {
-        console.log(error.response.data.errors);
-        setErrors(error.response.data.errors);
-      });
-  };
-
-  return (
-    <div id="signup">
-      <h1>Signup</h1>
-      <ul>
-        {errors.map((error) => (
-          <li key={error}>{error}</li>
-        ))}
-      </ul>
-      <form onSubmit={handleSubmit}>
-        <div>
-          Name: <input name="name" type="text" />
-        </div>
-        <div>
-          Email: <input name="email" type="email" />
-        </div>
-        <div>
-          Password: <input name="password" type="password" />
-        </div>
-        <div>
-          Password confirmation:{" "}
-          <input name="password_confirmation" type="password" />
-        </div>
-        <button type="submit">Signup</button>
-      </form>
-    </div>
-  );
-}
-
-const jwt = localStorage.getItem("jwt");
-if (jwt) {
-  axios.defaults.headers.common["Authorization"] = `Bearer ${jwt}`;
-}
-
-export function Login() {
-  const [errors, setErrors] = useState([]);
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    setErrors([]);
-    const params = new FormData(event.target);
-    axios
-      .post("http://localhost:3000/sessions", params)
-      .then((response) => {
-        console.log(response.data);
-        axios.defaults.headers.common["Authorization"] =
-          "Bearer " + response.data.jwt;
-        localStorage.setItem("jwt", response.data.jwt);
-        event.target.reset();
-        window.location.href = "/"; // Change this to hide a modal, redirect to a specific page, etc.
-      })
-      .catch((error) => {
-        console.log(error.response);
-        setErrors(["Invalid email or password"]);
-      });
-  };
-
-  return (
-    <div id="login">
-      <h1>Login</h1>
-      <ul>
-        {errors.map((error) => (
-          <li key={error}>{error}</li>
-        ))}
-      </ul>
-      <form onSubmit={handleSubmit}>
-        <div>
-          Email: <input name="email" type="email" />
-        </div>
-        <div>
-          Password: <input name="password" type="password" />
-        </div>
-        <button type="submit">Login</button>
-      </form>
-    </div>
-  );
-}
-
-export function LogoutLink() {
-  const handleClick = (event) => {
-    event.preventDefault();
-    delete axios.defaults.headers.common["Authorization"];
-    localStorage.removeItem("jwt");
-    window.location.href = "/";
-  };
-
-  return (
-    <a href="#" onClick={handleClick}>
-      Logout
-    </a>
   );
 }
